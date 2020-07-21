30018c4a61da9e275fc9dafcbd7fb7eccb34f02a
Sph22 <safwen.hafs@gmail.com>
2020-07-21 17:11:17 +0100

    modified game.js

--- src/game.js
+++ src/game.js
@@ -3,6 +3,12 @@ import inputHandler from "./input.js";
 import Ball from './ball.js';
 import Goal from "./goal.js";
 
+const GAMESTATE = {
+    MENU : 0,
+    RUNNING : 1, 
+    PAUSED : 2, 
+    OVER : 3,
+};
 
 export default class Game{
     constructor(gameWidth, gameHeight){
@@ -10,37 +16,36 @@ export default class Game{
         this.gameHeight = gameHeight;
         this.pitchWidth = 700;
         this.lives = 3;
-        const GAMESTATE = [
-            MENU = 0,
-            RUNNING = 1, 
-            PAUSED = 2, 
-            OVER = 3
-        ]
+
     }
     start(){
-        this.gameStatut  = GAMESTATE.RUNNING;
+        this.gameState = GAMESTATE.MENU;
         this.paddle = new Paddle(this);
         this.ball = new Ball(this);
         this.goal = new Goal(this, this.pitchWidth);
         this.gameObjects = [this.paddle, this.ball, this.goal];
 
 
-        new inputHandler(this.paddle);       
+        new inputHandler(this, this.paddle);       
     };
 
     update(dt){
+        if(this.gameState === GAMESTATE.PAUSED || this.gameState === GAMESTATE.MENU) return;
+
         this.gameObjects.forEach(object => object.update(dt));
         
     };
 
     draw(ctx){
         this.gameObjects.forEach(object => object.draw(ctx));
+
         //drawing the pitch borders
         ctx.beginPath();
         ctx.lineWidth = "6";
         ctx.strokeStyle = "whitesmoke";
         ctx.rect(700,0, 0, 700);
         ctx.stroke();
+
         //scoreSheet drawing 
         ctx.rect(700, 100, 100, 100);
         ctx.fillStyle = "white";
@@ -49,14 +54,36 @@ export default class Game{
         ctx.fillStyle = "Black";
         ctx.textAlign = "center";
         ctx.fillText(this.ball.score, 750, 160);
-        //lifes draw
+
+        //drawing the pause screen 
+        if(this.gameState === GAMESTATE.PAUSED){
+            ctx.rect(0, 0 ,800, 600);
+            ctx.fillStyle = "rgba(0,0,0,0.5)"
+            ctx.fill();
+            ctx.font = "50px courier";
+            ctx.fillStyle = "Whitesmoke";
+            ctx.textAlign = " center";
+            ctx.fillText("Game Paused", this.gameWidth / 2 , this.gameHeight /2 );
+        }
+        //drawing the menu
+        if(this.gameState === GAMESTATE.MENU){
+            ctx.rect(0, 0 ,800, 600);
+            ctx.fillStyle = "black"
+            ctx.fill();
+            ctx.font = "30px courier";
+            ctx.fillStyle = "Whitesmoke";
+            ctx.textAlign = " center";
+            ctx.fillText("Press Enter to start the game", this.gameWidth / 2 , this.gameHeight /2 );
+
+        }
       
     };
 
     togglePause(){
-        if(this.gameStatut === GAMESTATE.PAUSED) return;
-        else{
-            this.gameStatut === GAMESTATE.RUNNING;
+        if(this.gameState === GAMESTATE.PAUSED){
+            this.gameState = GAMESTATE.RUNNING
+        }else{
+            this.gameState = GAMESTATE.PAUSED;
         }
-    }
+    };
 }
